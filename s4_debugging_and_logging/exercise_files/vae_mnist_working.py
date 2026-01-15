8e0580911017fdc9fc6c19298470078b368b6fa7
Slupon <sergi.luponsantacana@gmail.com>
2026-01-15 16:24:03 +0100

    Profilers (without Pytorch profiler)

--- s4_debugging_and_logging/exercise_files/vae_mnist_working.py
+++ s4_debugging_and_logging/exercise_files/vae_mnist_working.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
@@ -25,8 +26,14 @@ epochs = 5
 # Data loading
 mnist_transform = transforms.Compose([transforms.ToTensor()])
 
-train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
-test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
+""" optimizing run (point 5 of module 13) """
+
+train_dataset = MNIST(dataset_path, train=True, download=True)
+train_dataset = TensorDataset(train_dataset.data.float() / 255.0, train_dataset.targets)
+test_dataset = MNIST(dataset_path, train=False, download=True)
+test_dataset = TensorDataset(test_dataset.data.float() / 255.0, test_dataset.targets)
+#train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
+#test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
